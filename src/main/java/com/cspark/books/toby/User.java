ed684a7b929471babfb0e8dfa57254b4be4e5962
pcspapa <pcspapa@gmail.com>
2015-11-30 11:39:04 +0900

    initial

---
+++ src/main/java/com/cspark/books/toby/User.java
@@ -0,0 +1,37 @@
+package com.cspark.books.toby;
+
+/**
+ * Created by cspark on 2015. 11. 25..
+ */
+public class User {
+
+    String id;
+
+    String name;
+
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/cspark/books/toby/UserDao.java
@@ -0,0 +1,65 @@
+package com.cspark.books.toby;
+
+import java.sql.*;
+
+/**
+ * Created by cspark on 2015. 11. 25..
+ */
+public class UserDao {
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost//projects/h2data/books/toby", "sa", "");
+
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+        ps.setString(1, user.getId      ());
+        ps.setString(2, user.getName    ());
+        ps.setString(3, user.getPassword());
+
+        ps.execute();
+
+        ps.close();
+        conn.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost//projects/h2data/books/toby", "sa", "");
+
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId      (rs.getString("id"      ));
+        user.setName    (rs.getString("name"    ));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId      ("cspark");
+        user.setName    ("cs 박"  );
+        user.setPassword("1111"  );
+
+        dao.add(user);
+
+        System.out.println("등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName    ());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+    }
+
+}
