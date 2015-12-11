edac7e3bba5fbb05d1c29927fc3d5279e3822762
pcspapa <pcspapa@gmail.com>
2015-12-11 14:39:44 +0900

    3.3 XML Application Context

--- src/main/java/com/cspark/books/toby/UserDaoTest.java
+++ src/main/java/com/cspark/books/toby/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.cspark.books.toby;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -11,7 +12,7 @@ import java.sql.SQLException;
 public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
