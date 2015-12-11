fa1ec2b3ff4665021b41b50a43ae846f3be87374
pcspapa <pcspapa@gmail.com>
2015-12-11 13:56:16 +0900

    3.1 Object Factory

--- src/main/java/com/cspark/books/toby/DaoFactory.java
+++ src/main/java/com/cspark/books/toby/DaoFactory.java
@@ -4,4 +4,11 @@ package com.cspark.books.toby;
  * Created by cspark on 2015. 12. 11..
  */
 public class DaoFactory {
+
+    public UserDao userDao() {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+        return userDao;
+    }
+
 }
