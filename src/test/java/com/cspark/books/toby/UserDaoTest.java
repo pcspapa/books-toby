c9b6ecfcc8c76a64fbe38e68579143db07442dfb
pcspapa <pcspapa@gmail.com>
2015-12-14 14:47:19 +0900

    2.4 JUnit with Spring

--- src/test/java/com/cspark/books/toby/UserDaoTest.java
+++ src/test/java/com/cspark/books/toby/UserDaoTest.java
@@ -2,9 +2,13 @@ package com.cspark.books.toby;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
@@ -14,19 +18,19 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by cspark on 2015. 11. 30..
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
 
+    @Autowired
     private UserDao dao;
+
     private User user1;
     private User user2;
     private User user3;
 
     @Before
     public void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        dao = context.getBean("userDao", UserDao.class);
-
         user1 = new User("cspark1", "비마중1", "1111");
         user2 = new User("cspark2", "비마중2", "1112");
         user3 = new User("cspark3", "비마중3", "1113");
