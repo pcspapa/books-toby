73d40e111ca68c3682e7a11b11ca33a339bf052c
pcspapa <pcspapa@gmail.com>
2015-12-14 21:48:42 +0900

    3.6.3 JdbcTemplate in Spring

--- src/main/java/com/cspark/books/toby/dao/UserDao.java
+++ src/main/java/com/cspark/books/toby/dao/UserDao.java
@@ -6,6 +6,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -17,80 +18,33 @@ public class UserDao {
 
     private JdbcTemplate jdbcTemplate;
 
-    private JdbcContext jdbcContext;
-
     private DataSource dataSource;
-
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(final User user) throws SQLException {
+    public void add(final User user) {
         jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES (?, ?, ?)", user.getId(), user.getName(), user.getPassword());
     }
 
-    public User get(String id) throws SQLException {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        User user = null;
-
-        try {
-            conn = dataSource.getConnection();
-            ps = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
-            ps.setString(1, id);
-
-            rs = ps.executeQuery();
-            if (rs.next()) {
-                user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-            }
-
-            if (user == null) throw new EmptyResultDataAccessException(1);
-
+    public User get(String id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE id = ?", new Object[] {id}, (resultSet, i) -> {
+            User user = new User();
+            user.setId(resultSet.getString("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
             return user;
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        });
     }
 
-    public void deleteAll() throws SQLException {
+    public void deleteAll() {
         jdbcTemplate.update("DELETE FROM users");
     }
 
-    public int getCount() throws SQLException {
-        return jdbcTemplate.query(connection -> {
-            return connection.prepareStatement("SELECT COUNT(*) FROM users");
-        }, resultSet -> {
-            resultSet.next();
-            return resultSet.getInt(1);
-        });
+    public int getCount() {
+        return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
     }
 
 }
