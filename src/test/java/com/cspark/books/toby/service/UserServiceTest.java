d8deb2e399797d823b6bcf9748859f821dfcc4d5
pcspapa <pcspapa@gmail.com>
2015-12-21 17:49:22 +0900

    6.6.2 트랜잭션 인터셉터와 트랜잭션 속성

--- src/test/java/com/cspark/books/toby/service/UserServiceTest.java
+++ src/test/java/com/cspark/books/toby/service/UserServiceTest.java
@@ -29,7 +29,7 @@ import static org.junit.Assert.fail;
  * Created by cspark on 2015. 12. 16..
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/aop-applicationContext.xml")
+@ContextConfiguration(locations = "/tx-applicationContext.xml")
 public class UserServiceTest {
 
     @Autowired
