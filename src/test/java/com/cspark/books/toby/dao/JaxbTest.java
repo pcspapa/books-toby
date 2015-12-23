efa0a7e716198fbbe81c4d0413bcc7f684aeb0a0
pcspapa <pcspapa@gmail.com>
2015-12-23 11:14:59 +0900

    7.2.1 XML 파일 매핑

---
+++ src/test/java/com/cspark/books/toby/dao/JaxbTest.java
@@ -0,0 +1,37 @@
+package com.cspark.books.toby.dao;
+
+import com.cspark.books.toby.sqlservice.jaxb.SqlType;
+import com.cspark.books.toby.sqlservice.jaxb.Sqlmap;
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by cspark on 2015. 12. 23..
+ */
+public class JaxbTest {
+
+    @Test
+    public void reqdSqlmap() throws Exception {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        JAXBContext context = JAXBContext.newInstance(contextPath);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(getClass().getResourceAsStream("test-sqlmap.xml"));
+
+        List<SqlType> sqlList = sqlmap.getSql();
+
+        assertThat(sqlList.size(), is(3));
+        assertThat(sqlList.get(0).getKey(), is("add"));
+        assertThat(sqlList.get(0).getValue(), is("INSERT"));
+        assertThat(sqlList.get(1).getKey(), is("get"));
+        assertThat(sqlList.get(1).getValue(), is("SELECT"));
+        assertThat(sqlList.get(2).getKey(), is("delete"));
+        assertThat(sqlList.get(2).getValue(), is("DELETE"));
+    }
+}
