908c8a8bdec46f2535fe247b179576d663026bfd
pcspapa <pcspapa@gmail.com>
2015-12-29 10:54:49 +0900

    7.3.1 OXM 서비스 추상화

---
+++ src/test/java/com/cspark/books/toby/dao/OxmTest.java
@@ -0,0 +1,46 @@
+package com.cspark.books.toby.dao;
+
+import com.cspark.books.toby.sqlservice.jaxb.SqlType;
+import com.cspark.books.toby.sqlservice.jaxb.Sqlmap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by cspark on 2015. 12. 29..
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/oxm-context.xml")
+public class OxmTest {
+
+    @Autowired
+    Unmarshaller unmarshaller;
+
+    @Test
+    public void unmarshalSqlMap() throws Exception {
+        Source xmlSource = new StreamSource(getClass().getResourceAsStream("test-sqlmap.xml"));
+
+        Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(xmlSource);
+        List<SqlType> sqlList = sqlmap.getSql();
+
+        assertThat(sqlList.size(), is(3));
+        assertThat(sqlList.get(0).getKey(), is("add"));
+        assertThat(sqlList.get(0).getValue(), is("INSERT"));
+        assertThat(sqlList.get(1).getKey(), is("get"));
+        assertThat(sqlList.get(1).getValue(), is("SELECT"));
+        assertThat(sqlList.get(2).getKey(), is("delete"));
+        assertThat(sqlList.get(2).getValue(), is("DELETE"));
+
+    }
+}
