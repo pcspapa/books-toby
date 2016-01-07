1154232d21841e943648c8677037fec4fabe7bbf
pcspapa <pcspapa@gmail.com>
2016-01-07 17:53:57 +0900

    1.1.3 Ioc 컨테이너 계층구조

--- src/test/java/com/cspark/books/toby/learningtest/spring/ioc/ApplicationContextTest.java
+++ src/test/java/com/cspark/books/toby/learningtest/spring/ioc/ApplicationContextTest.java
@@ -1,10 +1,12 @@
 package com.cspark.books.toby.learningtest.spring.ioc;
 
 import org.junit.Test;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
@@ -82,4 +84,28 @@ public class ApplicationContextTest {
         assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
     }
 
+    @Test(expected = BeanCreationException.class)
+    public void createContextWithoutParent() throws Exception {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("com/cspark/books/toby/learningtest/spring/ioc/childContext.xml");
+
+    }
+
+    @Test
+    public void contextHierachy() throws Exception {
+        ApplicationContext parent = new GenericXmlApplicationContext("com/cspark/books/toby/learningtest/spring/ioc/parentContext.xml");
+
+        GenericApplicationContext child = new GenericApplicationContext(parent);
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+        reader.loadBeanDefinitions("com/cspark/books/toby/learningtest/spring/ioc/childContext.xml");
+        child.refresh();
+
+        Printer printer = child.getBean("printer", Printer.class);
+        assertThat(printer, is(notNullValue()));
+
+        Hello hello = child.getBean("hello", Hello.class);
+        assertThat(hello, is(notNullValue()));
+
+        hello.print();
+        assertThat(printer.toString(), is("Hello Child"));
+    }
 }
